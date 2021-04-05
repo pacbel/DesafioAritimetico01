73ebc8c6bf31f46c3cfce8fc23b7fe7e465d7889
pacbel <suporte@sistemavirtual.com.br>
2021-04-05 11:29:50 -0300

    202104051129

---
+++ desafioAritmetico01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            double a = 0.0;
+            double b = 0.0;
+            double mediaP = 0.00000;
+
+            string entradaA = Console.ReadLine().Replace(',', '.');
+            string entradaB = Console.ReadLine().Replace(',', '.');
+
+            a = Convert.ToDouble(entradaA, CultureInfo.InvariantCulture);
+            b = Convert.ToDouble(entradaB, CultureInfo.InvariantCulture);
+
+            mediaP = (( a * 3.5) + ( b * 7.5)) / (3.5 + 7.5);
+
+            Console.WriteLine("MEDIA = {0}\n", mediaP.ToString("0.00000"));
+
+    }
+
+}
